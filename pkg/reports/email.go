737bd499363aeeb37f04644fa5b515d2fa70404a
Mitch Murphy Murphy <mitch.murphy@gmail.com>
2022-12-08 20:31:00 -0500

    Added package for sending emails via SMTP
    Former-commit-id: 0e657cf5bc9eb1eed0b4d695e37a2850663ddd5e

---
+++ pkg/reports/email.go
@@ -0,0 +1,50 @@
+package reports
+
+import (
+	"encoding/base64"
+	"net/smtp"
+	"strings"
+
+	utils "github.com/mkm29/vulncron/pkg/utils"
+)
+
+func SendMail(from, subject, body string, to []string) error {
+	r := strings.NewReplacer("\r\n", "", "\r", "", "\n", "", "%0a", "", "%0d", "")
+
+	c, err := smtp.Dial(utils.GetEnvVar("SMTP_HOST", "mailhub.utc.com:25"))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+	if err = c.Mail(r.Replace(from)); err != nil {
+		return err
+	}
+	for i := range to {
+		to[i] = r.Replace(to[i])
+		if err = c.Rcpt(to[i]); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	msg := "To: " + strings.Join(to, ",") + "\r\n" +
+		"From: " + from + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"Content-Type: text/html; charset=\"UTF-8\"\r\n" +
+		"Content-Transfer-Encoding: base64\r\n" +
+		"\r\n" + base64.StdEncoding.EncodeToString([]byte(body))
+
+	_, err = w.Write([]byte(msg))
+	if err != nil {
+		return err
+	}
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+	return c.Quit()
+}
